feee5b1cd83bb11a93895c09672ee2d56f0bdba6
jhodgens17 <81783731+jhodgens17@users.noreply.github.com>
2022-12-21 15:44:47 -0500

    Add files via upload
    Updating app to clean up dataframe and add title

--- streamlit.py
+++ streamlit.py
@@ -10,7 +10,14 @@ import streamlit as st
 df = pd.read_csv('activities.csv')
 df2 = df[df['Activity ID'].isin([8266253440, 8270420755])]
 df3 = df2[['Activity ID', 'Activity Date', 'Elapsed Time', 'Distance']]
-st.write(df3)
+df4 = df3.reset_index()
+df4['date'] = pd.to_datetime(df4['Activity Date']).dt.date
+df4['duration_minutes'] = df4['Elapsed Time']//60
+df4['duration_seconds'] = df4['Elapsed Time'] - df4['duration_minutes']*60
+df4['mileage']=df4['Distance']*0.621371
+df5 = df4.drop(['index', 'Activity ID', 'Elapsed Time', 'Distance', 'Activity Date'], axis=1)
+st.header('All run data:')
+st.write(df5)
 
 
 # In[ ]:
